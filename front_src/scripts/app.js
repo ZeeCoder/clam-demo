a1dd56b47207397df6c3ba85ffba8771bb6cd528
ZeeCoder <viktorhubert@Catherine.local>
2015-02-03 11:25:34 +0100

    Tried out the jquery deferred method.

--- front_src/scripts/app.js
+++ front_src/scripts/app.js
@@ -1,8 +1,9 @@
 var cutil = require('clam/core/util');
+var clam_module = require('clam/core/module');
 var clam_container = require('clam/core/container');
 var $ = require('jquery');
 var highlighter = require('./clam_module/highlighter');
-var dynamic = require('./clam_module/dynamic');
+var dynamic_ext = require('./clam_module/dynamic_ext');
 var dynamic_config = require('./conf/dynamic_config');
 var message = require('./clam_module/message');
 var highlighter_activator = require('./clam_module/highlighter_activator');
@@ -14,4 +15,4 @@ cutil.createPrototypes(message, {fadeOutTime: 300});
 cutil.createPrototypes(highlighter, {}, $('#highlighter-1'));
 cutil.createPrototypes(highlighter_activator);
 cutil.createPrototypes(clam_scroller);
-cutil.createPrototypes(dynamic, dynamic_config);
+cutil.createPrototypes(dynamic_ext, dynamic_config);
--- front_src/scripts/clam_module/dynamic.js
+++ front_src/scripts/clam_module/dynamic.js
@@ -8,7 +8,7 @@ var $ = require('jquery');
 
 var settings = {
     conf: {
-        prototypeHTML: '<div class="jsm-dynamic b-dynamic"> <a href="javascript: void(0)" class="jsm-dynamic__add-embedded-btn">New embedded</a> | <a href="javascript: void(0)" class="jsm-dynamic__add-sibling-btn">New sibling</a> | <a href="javascript: void(0)" class="jsm-dynamic__toggle-highlight">Highlight</a> <div class="b-dynamic__additional-modules jsm-dynamic__additional-modules"></div> </div>',
+        prototypeHTML: '<div class="jsm-dynamic b-dynamic"> <a href="javascript: void(0)" class="jsm-dynamic__add-embedded-btn">New embedded</a> | <a href="javascript: void(0)" class="jsm-dynamic__add-sibling-btn">New sibling</a> | <a href="javascript: void(0)" class="jsm-dynamic__toggle-highlight">Highlight</a> | <a href="javascript: void(0)" class="jsm-dynamic__destroy">Destroy</a> <div class="b-dynamic__additional-modules jsm-dynamic__additional-modules"></div> </div>',
         allowAddSibling: true,
         allowAddEmbedded: true
     }
@@ -17,10 +17,11 @@ var settings = {
 
 function Dynamic($jQObj, conf) {
     //var self = this;
+    console.log('Dynamic');
     clam_module.apply(this, [$jQObj, settings, conf]);
     this.expose();
 
-    this.moduleModifier = new modifier(this.module.$object, this.module.name);
+    this.moduleModifier = new modifier(this.module.$object, 'dynamic');
 
     if (this.module.conf.allowAddEmbedded) {
         this.getHook('add-embedded-btn').on('click', $.proxy(this.addEmbedded, this));
@@ -33,14 +34,27 @@ function Dynamic($jQObj, conf) {
     } else {
         this.getHook('add-sibling-btn').remove();
     }
+
+    this.getHook('destroy').on('click', $.proxy(this.destroy, this));
     
     this.getHook('toggle-highlight').on('click', $.proxy(this.toggleHighlight, this));
 }
 
 inherits(Dynamic, clam_module);
 
+// Dynamic.prototype.destroy = function() {
+//     clam_module.prototype.destroy.call(this, false);
+// };
+
 Dynamic.prototype.addEmbedded = function() {
-    this.addAdditionalModule();
+    var self = this;
+    $.when(this.triggerEvent('add'))
+    .fail(function() {
+        console.log('ERRROR');
+    })
+    .always(function() {
+        self.addAdditionalModule();
+    });
 };
 
 Dynamic.prototype.addSibling = function() {
@@ -59,7 +73,7 @@ Dynamic.prototype.addAdditionalModule = function(asSibling) {
         this.getHook('additional-modules').append($embeddedModul);
     }
     
-    cutil.createPrototypes(Dynamic, dynamic_config, $embeddedModul);
+    cutil.createPrototypes(this.constructor, dynamic_config, $embeddedModul);
 };
 
 module.exports = Dynamic;
